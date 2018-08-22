9420bcf6733b83a8f978a94fd8573641ea81686a
Jason <jasonlimowkwan@gmail.com>
2018-08-22 18:32:13 +0400

    player vs enemy js logic

--- js/gamemanager.js
+++ js/gamemanager.js
@@ -12,14 +12,14 @@ let GameManager = {
 			  player = new Player(classType, 100, 0, 100, 150, 200);
 			  break;
 			case "Mage":
-			  player = new Player(classType, 800, 0, 50, 200, 50);
+			  player = new Player(classType, 100, 0, 50, 200, 50);
 			  break;
 			case "Hunter":
 			  player = new Player(classType, 200, 0, 50, 200, 100);
 			  break;
 		}
 		let getInterface = document.querySelector(".interface");
-		getInterface.innerHTML = '<img src="img/avatar-player/' + classType.toLowerCase() + '.png" class="img-avatar"><div><h3>' + classType + '</h3><p>Health: ' + player.health + '</p><p>Mana: ' + player.mana + '</p><p>Strength: ' + player.strength + '</p><p>Agility: ' + player.agility + '</p><p>Speed: ' + player.speed + '</p></div>';
+		getInterface.innerHTML = '<img src="img/avatar-player/' + classType.toLowerCase() + '.png" class="img-avatar"><div><h3>' + classType + '</h3><p class="health-player">Health: ' + player.health + '</p><p>Mana: ' + player.mana + '</p><p>Strength: ' + player.strength + '</p><p>Agility: ' + player.agility + '</p><p>Speed: ' + player.speed + '</p></div>';
 
 	},
 	setPreFight: function() {
@@ -49,6 +49,6 @@ let GameManager = {
 		}
 		getHeader.innerHTML = '<p>Task: Choose your move!</p>';
 		getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>';
-		getEnemy.innerHTML = '<img src="img/avatar-enemies/' + enemy.enemyType.toLowerCase() + '.png" class="img-avatar"><div><h3>' + enemy.enemyType + '</h3><p>Health: ' + enemy.health + '</p><p>Mana: ' + enemy.mana + '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
+		getEnemy.innerHTML = '<img src="img/avatar-enemies/' + enemy.enemyType.toLowerCase() + '.png" class="img-avatar"><div><h3>' + enemy.enemyType + '</h3><p class="health-enemy">Health: ' + enemy.health + '</p><p>Mana: ' + enemy.mana + '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
 	}
 }
